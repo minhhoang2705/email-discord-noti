888ca4756c5108fb66a11f905742a77a72a56b8a
minhtran-213 <minhtran21301@gmail.com>
2023-02-07 21:32:52 +0700

    add new comment

--- email_messenger_chatbot.py
+++ email_messenger_chatbot.py
@@ -38,6 +38,7 @@ def send_to_messenger(subject, body):
     graph = facebook.GraphAPI(
         access_token=access_token)
 
+    #Cannot send to Messenger
     # Send the message to Messenger
     graph.put_object("me", message=subject + "\n" +
                      body, connection_name="message")
